9573b555ea874bb3660e8271ff8c972a9f933bef
Robert Basson <robertba@bbd.co.za>
2019-09-19 06:46:43 +0200

    Initial Project

---
+++ desktop/src/com/bbd/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.bbd.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.bbd.game.BBDHackathon;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new BBDHackathon(), config);
+	}
+}
